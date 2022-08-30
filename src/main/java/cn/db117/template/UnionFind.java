4cfb156b02f0f0f81d6fe037abc5a06b8ae5db4d
db117 <z351622948@163.com>
2022-08-30 16:14:20 +0800

    并查集模版

---
+++ src/main/java/cn/db117/template/UnionFind.java
@@ -0,0 +1,57 @@
+package cn.db117.template;
+
+/**
+ * 并查集
+ *
+ * @author db117
+ * @since 2022/8/30 16:13
+ **/
+public class UnionFind {
+    // 连通分量
+    int count;
+    // 父节点
+    int[] parent;
+
+    public UnionFind(int n) {
+        count = n;
+        parent = new int[n];
+
+        // 初始父节点都是自己
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public void union(int x, int y) {
+        int xp = find(x);
+        int yp = find(y);
+        if (xp == yp) {
+            return;
+        }
+        if (xp < yp) {
+            parent[yp] = xp;
+        } else {
+            parent[xp] = yp;
+        }
+        // 连通分量
+        count--;
+    }
+
+    public int find(int n) {
+        while (parent[n] != n) {
+            // 路径压缩
+            parent[n] = parent[parent[n]];
+            n = parent[n];
+        }
+        return n;
+    }
+
+    public boolean connected(int x, int y) {
+        return find(y) == find(x);
+    }
+
+    public int count() {
+        return count;
+    }
+
+}
