0f3bc50c464ad65dda57cbb3e9aa27b3d3fd026a
db117 <z351622948@163.com>
2022-09-13 18:07:39 +0800

    2404.出现最频繁的偶数元素.most-frequent-even-element

---
+++ src/main/java/cn/db117/template/DiffArray.java
@@ -0,0 +1,32 @@
+package cn.db117.template;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * 差分数组
+ * 处理区间更新的问题
+ * 可以理解为公交车上下车,车上最多多少人的问题
+ *
+ * @author db117
+ * @since 2022/9/13 18:06
+ **/
+public class DiffArray {
+    public int diffArray(int[][] intervals) {
+        // 差分数组
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for (int[] interval : intervals) {
+            treeMap.put(interval[0], treeMap.getOrDefault(interval[0], 0) + 1);
+            treeMap.put(interval[1] + 1, treeMap.getOrDefault(interval[1] + 1, 0) - 1);
+        }
+
+        int max = 0;
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
+            Integer count = entry.getValue();
+            sum += count;
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
