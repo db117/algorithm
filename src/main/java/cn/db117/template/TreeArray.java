a35d1d1ee54a1981ca0bd10a67cdad1995c2fa8b
db117 <z351622948@163.com>
2022-09-29 19:03:07 +0800

    树状数组

---
+++ src/main/java/cn/db117/template/TreeArray.java
@@ -0,0 +1,54 @@
+package cn.db117.template;
+
+/**
+ * 树状数组
+ *
+ * @author db117
+ * @since 2022/9/29 18:18
+ **/
+public class TreeArray {
+    // 下标从 1 开始
+    int[] tree;
+    int[] nums;
+
+    public TreeArray(int[] nums) {
+        this.nums = nums;
+        tree = new int[nums.length + 10];
+        for (int i = 0; i < nums.length; i++) {
+            add(i + 1, nums[i]);
+        }
+    }
+
+    /**
+     * 最后一个 1
+     */
+    public int lowBit(int i) {
+        return i & -i;
+    }
+
+    /**
+     * 在指定位置添加值
+     *
+     * @param x 数组位置(从 1 开始)
+     * @param v 增加的值
+     */
+    public void add(int x, int v) {
+        for (int i = x; i <= nums.length; i += lowBit(i)) {
+            tree[i] += v;
+        }
+    }
+
+    /**
+     * 查询[1,x]的和
+     *
+     * @param x 数组位置(从 1 开始)
+     */
+    public int query(int x) {
+        int ans = 0;
+        for (int i = x; i > 0; i -= lowBit(i)) {
+            ans += tree[i];
+        }
+        return ans;
+    }
+
+}
--- src/main/java/cn/db117/template/package-info.java
+++ src/main/java/cn/db117/template/package-info.java
@@ -2,5 +2,28 @@
  * @author db117
  * @since 2022/8/25 11:57
  * 算法模板
+ * dp
+ * 数位 dp  {@link cn.db117.template.dp.NumberBitDP}
+ * <p>
+ * 线段树
+ * 单点修改{@link cn.db117.template.segment_tree.SegmentTreeSinge}
+ * 区间修改求最大值{@link cn.db117.template.segment_tree.SegmentTreeRangeMax}
+ * 区间修改,区间查询和{@link cn.db117.template.segment_tree.SegmentTreeRangeSum}
+ * 区间覆盖,区间查询最大值{@link cn.db117.template.segment_tree.SegmentTreeRangeOverrideMax}
+ * 区间覆盖,区间查询和{@link cn.db117.template.segment_tree.SegmentTreeRangeOverrideSum}
+ * <p>
+ * 二分搜索{@link cn.db117.template.BinarySearch}
+ * <p>
+ * 差分数组
+ * 区间修改,求过程最大值{@link cn.db117.template.DiffArray}
+ * <p>
+ * 拓扑排序 {@link cn.db117.template.TopologicalSort}
+ * <p>
+ * 字典树 {@link  cn.db117.template.Trie}
+ * <p>
+ * 并查集 {@link  cn.db117.template.UnionFind}
+ * <p>
+ * 树状数组
+ * {@link cn.db117.template.TreeArray}
  **/
 package cn.db117.template;
