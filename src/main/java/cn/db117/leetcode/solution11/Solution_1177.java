36721f9156fcc61e65fbb4a8de9890975fc68db0
db117 <z351622948@163.com>
2023-06-15 13:47:07 +0800

    1177.构建回文串检测.can-make-palindrome-from-substring

---
+++ src/main/java/cn/db117/leetcode/solution11/Solution_1177.java
@@ -0,0 +1,95 @@
+
+
+//给你一个字符串 s，请你对 s 的子串进行检测。 
+//
+// 每次检测，待检子串都可以表示为 queries[i] = [left, right, k]。我们可以 重新排列 子串 s[left], ..., s[
+//right]，并从中选择 最多 k 项替换成任何小写英文字母。 
+//
+// 如果在上述检测过程中，子串可以变成回文形式的字符串，那么检测结果为 true，否则结果为 false。 
+//
+// 返回答案数组 answer[]，其中 answer[i] 是第 i 个待检子串 queries[i] 的检测结果。 
+//
+// 注意：在替换时，子串中的每个字母都必须作为 独立的 项进行计数，也就是说，如果 s[left..right] = "aaa" 且 k = 2，我们只能替换
+//其中的两个字母。（另外，任何检测都不会修改原始字符串 s，可以认为每次检测都是独立的） 
+//
+// 
+//
+// 示例： 
+//
+// 输入：s = "abcda", queries = [[3,3,0],[1,2,0],[0,3,1],[0,3,2],[0,4,1]]
+//输出：[true,false,false,true,true]
+//解释：
+//queries[0] : 子串 = "d"，回文。
+//queries[1] : 子串 = "bc"，不是回文。
+//queries[2] : 子串 = "abcd"，只替换 1 个字符是变不成回文串的。
+//queries[3] : 子串 = "abcd"，可以变成回文的 "abba"。 也可以变成 "baab"，先重新排序变成 "bacd"，然后把 "cd" 
+//替换为 "ab"。
+//queries[4] : 子串 = "abcda"，可以变成回文的 "abcba"。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length, queries.length <= 10^5 
+// 0 <= queries[i][0] <= queries[i][1] < s.length 
+// 0 <= queries[i][2] <= s.length 
+// s 中只有小写英文字母 
+// 
+//
+// Related Topics 位运算 数组 哈希表 字符串 前缀和 👍 123 👎 0
+
+
+package cn.db117.leetcode.solution11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1177.构建回文串检测.can-make-palindrome-from-substring
+ *
+ * @author db117
+ * @since 2023-06-15 11:28:37
+ **/
+
+public class Solution_1177 {
+    public static void main(String[] args) {
+        Solution solution = new Solution_1177().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<Boolean> canMakePaliQueries(String s, int[][] queries) {
+            char[] chars = s.toCharArray();
+            // 每个字符出现的次数进行前缀和
+            int[][] pre = new int[26][chars.length + 1];
+            for (int i = 0; i < chars.length; i++) {
+                for (int j = 0; j < 26; j++) {
+                    pre[j][i + 1] = pre[j][i];
+                }
+                pre[chars[i] - 'a'][i + 1]++;
+            }
+            List<Boolean> ans = new ArrayList<>(queries.length);
+            for (int[] query : queries) {
+                int cur = 0;
+                for (int i = 0; i < 26; i++) {
+                    // 当前字符出现的次数
+                    int count = pre[i][query[1] + 1] - pre[i][query[0]];
+                    // 出现奇数次
+                    if ((count & 1) == 1) {
+                        cur++;
+                    }
+                }
+                if (cur / 2 <= query[2]) {
+                    ans.add(true);
+                } else {
+                    ans.add(false);
+                }
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
