a19d27581e8c1bb5c9e272471651d0b5fe813992
db117 <z351622948@163.com>
2024-03-26 23:34:12 +0800

    3092.最高频率的 ID.most-frequent-ids

---
+++ src/main/java/cn/db117/leetcode/solution30/Solution_3092.java
@@ -0,0 +1,109 @@
+
+
+//你需要在一个集合里动态记录 ID 的出现频率。给你两个长度都为 n 的整数数组 nums 和 freq ，nums 中每一个元素表示一个 ID ，对应的 
+//freq 中的元素表示这个 ID 在集合中此次操作后需要增加或者减少的数目。 
+//
+// 
+// 增加 ID 的数目：如果 freq[i] 是正数，那么 freq[i] 个 ID 为 nums[i] 的元素在第 i 步操作后会添加到集合中。 
+// 减少 ID 的数目：如果 freq[i] 是负数，那么 -freq[i] 个 ID 为 nums[i] 的元素在第 i 步操作后会从集合中删除。 
+// 
+//
+// 请你返回一个长度为 n 的数组 ans ，其中 ans[i] 表示第 i 步操作后出现频率最高的 ID 数目 ，如果在某次操作后集合为空，那么 ans[
+//i] 为 0 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：nums = [2,3,2,1], freq = [3,2,-3,1] 
+// 
+//
+// 输出：[3,3,2,2] 
+//
+// 解释： 
+//
+// 第 0 步操作后，有 3 个 ID 为 2 的元素，所以 ans[0] = 3 。 第 1 步操作后，有 3 个 ID 为 2 的元素和 2 个 ID 为
+// 3 的元素，所以 ans[1] = 3 。 第 2 步操作后，有 2 个 ID 为 3 的元素，所以 ans[2] = 2 。 第 3 步操作后，有 2 个 
+//ID 为 3 的元素和 1 个 ID 为 1 的元素，所以 ans[3] = 2 。 
+//
+// 示例 2： 
+//
+// 
+// 输入：nums = [5,5,3], freq = [2,-2,1] 
+// 
+//
+// 输出：[2,0,1] 
+//
+// 解释： 
+//
+// 第 0 步操作后，有 2 个 ID 为 5 的元素，所以 ans[0] = 2 。 第 1 步操作后，集合中没有任何元素，所以 ans[1] = 0 。 
+//第 2 步操作后，有 1 个 ID 为 3 的元素，所以 ans[2] = 1 。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length == freq.length <= 10⁵ 
+// 1 <= nums[i] <= 10⁵ 
+// -10⁵ <= freq[i] <= 10⁵ 
+// freq[i] != 0 
+// 输入保证任何操作后，集合中的元素出现次数不会为负数。 
+// 
+//
+// Related Topics 数组 哈希表 有序集合 堆（优先队列） 👍 5 👎 0
+
+
+package cn.db117.leetcode.solution30;
+
+import java.util.*;
+
+/**
+ * 3092.最高频率的 ID.most-frequent-ids
+ *
+ * @author db117
+ * @since  2024-03-26 23:32:54
+ **/
+
+  public class Solution_3092{
+      public static void main(String[] args) {
+           Solution solution = new Solution_3092().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+      class Solution {
+          public long[] mostFrequentIDs(int[] nums, int[] freq) {
+              int n = nums.length;
+              long[] ans = new long[n];
+              // 出现的次数->数字
+              TreeMap<Long, Set<Integer>> countMax = new TreeMap<>();
+              // 数字->出现次数
+              Map<Integer, Long> count = new HashMap<>();
+              for (int i = 0; i < n; i++) {
+                  // 之前的次数
+                  Long preCount = count.getOrDefault(nums[i], 0L);
+                  // 新的次数
+                  long newCount = preCount + freq[i];
+
+                  count.put(nums[i], newCount);
+
+                  countMax.computeIfAbsent(newCount, k -> new HashSet<>())
+                          .add(nums[i]);
+
+                  Set<Integer> pre = countMax.get(preCount);
+                  if (pre != null) {
+                      pre.remove(nums[i]);
+                      if (pre.isEmpty()) {
+                          countMax.remove(preCount);
+                      }
+                  }
+
+                  ans[i] = countMax.lastKey();
+              }
+              return ans;
+          }
+
+      }
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
