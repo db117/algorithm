16dbbde72abe9ebbe9c89db2f76ef09713eb1f4a
zhangdabing <zhangdabing@autohome.com.cn>
2025-09-26 15:53:29 +0800

    3683.完成一个任务的最早时间.earliest-time-to-finish-one-task

---
+++ src/main/java/cn/db117/leetcode/solution36/Solution_3683.java
@@ -0,0 +1,77 @@
+
+
+//给你一个二维整数数组 tasks，其中 tasks[i] = [si, ti]。 
+//
+// 数组中的每个 [si, ti] 表示一个任务，该任务的开始时间为 si，完成该任务需要 ti 个时间单位。 
+//
+// 返回至少完成一个任务的最早时间。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入： tasks = [[1,6],[2,3]] 
+// 
+//
+// 输出： 5 
+//
+// 解释： 
+//
+// 第一个任务从时间 t = 1 开始，并在 1 + 6 = 7 时完成。第二个任务在时间 t = 2 开始，并在 2 + 3 = 5 时完成。因此，最早完成
+//的任务在时间 5。 
+//
+// 示例 2： 
+//
+// 
+// 输入： tasks = [[100,100],[100,100],[100,100]] 
+// 
+//
+// 输出： 200 
+//
+// 解释： 
+//
+// 三个任务都在时间 100 + 100 = 200 时完成。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= tasks.length <= 100 
+// tasks[i] = [si, ti] 
+// 1 <= si, ti <= 100 
+// 
+//
+// 👍 0 👎 0
+
+
+package cn.db117.leetcode.solution36;
+
+/**
+ * 3683.完成一个任务的最早时间.earliest-time-to-finish-one-task
+ *
+ * @author db117
+ * @since 2025-09-26 15:50:42
+ **/
+
+public class Solution_3683 {
+    public static void main(String[] args) {
+        Solution solution = new Solution_3683().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int earliestTime(int[][] tasks) {
+            int ans = tasks[0][1] + tasks[0][0];
+            for (int[] task : tasks) {
+                int start = task[0];
+                int end = task[1];
+                ans = Math.min(ans, start + end);
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
